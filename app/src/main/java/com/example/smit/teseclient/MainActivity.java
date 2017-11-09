520c9e41984151bd7058b61e517be77fe078990f
Alexey Gylytskyy <alexeygyl@gmail.com>
2017-11-09 13:25:30 -0100

    (Arch): SlidingTabs

--- app/src/main/java/com/example/smit/teseclient/MainActivity.java
+++ app/src/main/java/com/example/smit/teseclient/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.smit.teseclient;
 
+import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import com.example.smit.teseclient.Adapter.ViewPagerAdapter;
+import com.example.smit.teseclient.Google.SlidingTabLayout;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    ConstraintLayout mainLayout;
+    Drawable DmainView;
+    CharSequence Titles[]={"List","Music","Coluns"};
+    int Numboftabs =3 ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mainLayout = (ConstraintLayout)findViewById(R.id.Main);
+        try {
+            DmainView  = Drawable.createFromStream(getAssets().open("icons/mainbg2.jpg"), null);
+            mainLayout.setBackground(DmainView);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
+
+
     }
+
+
+
+
 }
