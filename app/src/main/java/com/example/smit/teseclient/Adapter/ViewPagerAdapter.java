7642b1ab7d8a7b930f84463265f504a085cb0468
Alexey Gylytskyy <alexeygyl@gmail.com>
2017-11-09 13:48:37 -0100

    (Arch): SlidingTabs

---
+++ app/src/main/java/com/example/smit/teseclient/Adapter/ViewPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.example.smit.teseclient.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.smit.teseclient.Fragments.Coluns;
+import com.example.smit.teseclient.Fragments.List;
+import com.example.smit.teseclient.Fragments.Music;
+
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
+    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+
+
+    // Build a Constructor and assign the passed Values to appropriate values in the class
+    public ViewPagerAdapter(FragmentManager fm,CharSequence mTitles[], int mNumbOfTabsumb) {
+        super(fm);
+
+        this.Titles = mTitles;
+        this.NumbOfTabs = mNumbOfTabsumb;
+
+    }
+
+    //This method return the fragment for the every position in the View Pager
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                List list = new List();
+                return list;
+            case 1:
+                Music music = new Music();
+                return music;
+            case 2:
+                Coluns coluns = new Coluns();
+                return coluns;
+        }
+
+        return null;
+    }
+
+    // This method return the titles for the Tabs in the Tab Strip
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return Titles[position];
+    }
+
+    // This method return the Number of tabs for the tabs Strip
+
+    @Override
+    public int getCount() {
+        return NumbOfTabs;
+    }
+}
